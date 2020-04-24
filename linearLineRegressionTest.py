8845036087a2d2a9e58754c4784c36dd5e2c128a
Alex <37157369+alex938@users.noreply.github.com>
2020-04-24 14:14:48 +0100

    Update linearLineRegressionTest.py

--- linearLineRegressionTest.py
+++ linearLineRegressionTest.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 #pd.read_excel for excel docs
 dataset = pd.read_csv('Salary_Data.csv')
@@ -15,9 +16,13 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, rando
 
 #print(x_test)
 
+#simple line regressor 
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+
 y_pred = regressor.predict(x_test)
 
-#plot training data
+#plot training data. Result should be a nice line in which we can predict an expected result
 plt.scatter(x_train, y_train, color = 'red')
 plt.plot(x_train, regressor.predict(x_train), color = 'blue')
 plt.title("Salary vs Experience (Trg Set)")
@@ -27,6 +32,7 @@ plt.show()
 
 #plot test data
 plt.scatter(x_test, y_test, color = 'red')
+#can plot the training or test data, they should be exactly the same
 plt.plot(x_train, regressor.predict(x_train), color = 'blue')
 plt.title("Salary vs Experience (Test Set)")
 plt.xlabel("Years of Exp")
